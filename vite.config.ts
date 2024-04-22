0502d887e5930feb6f56b0d39d5c38f4080cf035
Ken Kieu <minhcanhdn0704@gmail.com>
2024-04-22 14:21:50 -0700

    Add auto import style

--- vite.config.ts
+++ vite.config.ts
@@ -2,39 +2,41 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dts from 'vite-plugin-dts'
 import { resolve } from 'pathe'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
 
 export default defineConfig({
-    resolve: {
-        alias: {
-            '/@': resolve(__dirname, './src'),
-        },
+  resolve: {
+    alias: {
+      '/@': resolve(__dirname, './src'),
     },
-    plugins: [
-        vue(),
-        dts({
-            insertTypesEntry: true,
-        }),
-    ],
-    build: {
-        lib: {
-            entry: resolve(__dirname, 'src/components/index.ts'),
-            name: 'vuetify-navigation-drawer-resize',
-            fileName: 'vuetify-navigation-drawer-resize',
-        },
-        watch: {
-            include: [resolve(__dirname, 'src')],
-        },
-        rollupOptions: {
-            external: ['vue'],
-            output: {
-                exports: 'named',
-                globals: {
-                    vue: 'Vue',
-                },
-            },
-        },
+  },
+  plugins: [
+    vue(),
+    dts({
+      insertTypesEntry: true,
+    }),
+    libInjectCss(),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/components/index.ts'),
+      name: 'vuetify-navigation-drawer-resize',
+      fileName: 'vuetify-navigation-drawer-resize',
     },
-    optimizeDeps: {
-        exclude: ['vue'],
+    watch: {
+      include: [resolve(__dirname, 'src')],
     },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        exports: 'named',
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    exclude: ['vue'],
+  },
 })
